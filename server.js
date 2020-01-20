004c8083f845d44bb9060b35e739e853ccd7f83f
Phillip Jones <ptj92e@gmail.com>
2020-01-19 20:02:45 -0600

    Completed working routes on server

---
+++ server.js
@@ -0,0 +1,20 @@
+let express = require("express");
+let path = require("path");
+
+let app = express();
+let PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "./index.html"));
+});
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "./notes.html"));
+});
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
