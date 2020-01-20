796ed88ddbd28176fc7e9d7f1a3ee58c7e131346
Phillip Jones <ptj92e@gmail.com>
2020-01-19 21:17:34 -0600

    Fixed static resources

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "./notes.html"));
 });
 
+app.use("/assets", express.static("assets"));
+
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
 });
